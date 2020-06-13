7acecfae7ed23a763f46093165d1716942787537
chuanjiao10 <1328486072@qq.com>
2020-06-13 15:08:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# kasini3000_agent_win10
+kasini3000_agent_win10 is powershell + winrm
+
+use "setup_winrm_win10.bat" to setup winrm in win10
+
+url£º
+https://github.com/kasini3000/kasini3000_agent_win10.git
---
+++ setup_winrm_win10.bat
@@ -0,0 +1,8 @@
+﻿echo "管理员权限运行"
+C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy bypass -command "Enable-PSRemoting -SkipNetworkProfileCheck -Force"
+
+C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy bypass -command "Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-NoScope" -RemoteAddress Any"
+
+C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy bypass -command "Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP" -RemoteAddress Any"
+
+C:\windows\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy bypass -command "restart-service -Name WinRM -Force"
